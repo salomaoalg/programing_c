bdb8a69abec452d521fcedf3a78479013716b247
Salomao Souza <sa.salomao@hotmail.com>
2026-04-20 20:58:49 +0100

    Hello Word

---
+++ 1.1_my_first_code_c/1.1_my_first_code_c.c
@@ -0,0 +1,17 @@
+// This is my first code in C
+
+#include <stdio.h>
+void main() {
+	/*
+	SALOMAO SOUZA 
+	DATE 20/04/2026
+	PORTUGAL-GUIMARAES-TAIPAS
+	*/
+	
+	// This is my first code in C
+
+	printf("Hello Word!!\n");
+	printf("Testing\nSpaces\nWith N");
+	(void)getchar();
+	return 0;
+}
